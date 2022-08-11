8b3d282f158852d76c23d9e0678f5d3faca39114
Ichu23 <107046961+Ichu23@users.noreply.github.com>
2022-08-12 01:39:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -99,7 +99,7 @@ async def next_page(bot, query):
         off_set = offset - 10
     if n_offset == 0:
         btn.append(
-            [InlineKeyboardButton("⏪ BACK", callback_data=f"next_{req}_{key}_{off_set}"),
+            [InlineKeyboardButton("BACK", callback_data=f"next_{req}_{key}_{off_set}"),
              InlineKeyboardButton(f"{round(int(offset)/10)+1} - {round(total/10)}",callback_data="pages")]
         )
         btn.append(
